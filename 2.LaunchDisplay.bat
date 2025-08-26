339f5a6cdaad5fdf5ff2c6c555fbdce736e72e93
HomePC-005 <myhtpc005@gmail.com>
2025-08-26 11:34:00 +0800

    Update 2.LaunchDisplay.bat

--- 2.LaunchDisplay.bat
+++ 2.LaunchDisplay.bat
@@ -1,2 +1,3 @@
 @echo off
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\ChromeKiosk" --kiosk --autoplay-policy=no-user-gesture-required --no-first-run --disable-infobars --disable-session-crashed-bubble --autoplay-policy=no-user-gesture-required http://10.78.115.21:5000/display
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="C:\ChromeKiosk" --kiosk --autoplay-policy=no-user-gesture-required --no-first-run --disable-infobars --disable-session-crashed-bubble --autoplay-policy=no-user-gesture-required http://10.77.235.225:5000/display
